8aeba3af1dd0d600d308669113f24da1210a4321
mcbtay <mcbtay@failfactory.(none)>
2012-09-14 00:54:03 -0400

    actually made the preferences window a popup instead of a new window... close/cancel work

--- preferences_window.py
+++ preferences_window.py
@@ -1,19 +1,28 @@
 import gtk
 
 class PreferencesWindow:
-    def DestroyWindow(caller_widget):
-        window.destroy()
+
+    def DestroyWindow(self, caller_widget):
+        self.window.destroy()
+        
+    def CreateBottomButtons(self):
+        hbox = gtk.HBox(False, 0)
+
+        save = gtk.Button(stock = gtk.STOCK_SAVE)
+        cancel = gtk.Button(stock = gtk.STOCK_CANCEL)
+        cancel.connect("clicked", self.DestroyWindow)
+        
+        hbox.pack_start(save, False)
+        hbox.pack_start(cancel, False)
+        self.window.add(hbox)
         
     def __init__(self):
-        window = gtk.Window()
+        self.window = gtk.Window()
+        self.window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_MENU)
 
-        window.set_position(gtk.WIN_POS_CENTER)
-        window.resize(150, 300)
-
-        window.connect("destroy", self.DestroyWindow)
-
-        window.show_all()
-    
-    
-    
+        self.window.set_position(gtk.WIN_POS_CENTER)
+        self.window.resize(150, 300)
         
+        self.window.connect("destroy", self.DestroyWindow)
+        self.CreateBottomButtons()
+        self.window.show_all()
