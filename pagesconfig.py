b8a858f69da6af1e9307327faac8c950f84786ac
rsmitty <rsmitty1025@gmail.com>
2012-09-13 22:07:34 -0400

    removing pagesconfig.py. no longer necessary

--- pagesconfig.py
+++
@@ -1,9 +0,0 @@
-pages = dict()
-pages['sabnzbd+'] = 'http://192.168.0.17:8080'
-pages['sick beard'] = 'http://localhost:8081/home/'
-pages['couch potato'] = 'http://192.168.0.17:5000/movie/'
-pages['headphones'] = 'http://192.168.0.17:8181'
-pages['nzbmatrix'] = 'http://nzbmatrix.com/index.php'
-pages['dognzb'] = 'https://dognzb.cr/browse';
-pages['nzbs.org'] = 'http://nzbs.org';
-
